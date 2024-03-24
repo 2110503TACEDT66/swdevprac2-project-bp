cc33750c417e525a80048d14efdd382138ac59be
Phudit Pumcharern <bigphudit2547@gmail.com>
2024-03-24 10:40:22 +0700

    Remove waiting

--- src/libs/getHospitals.tsx
+++ src/libs/getHospitals.tsx
@@ -1,7 +1,6 @@
 const fetch = require('node-fetch');
 
 export default async function getHospitals() {
-    await new Promise( (resolve) => setTimeout(resolve, 1000) );
 
     const response = await fetch("https://vaccine-app-backend.vercel.app/api/v1/hospitals");
 

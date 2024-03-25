ca2d4ebb6d246994c9d5416703f2a943dcb64066
Phudit Pumcharern <bigphudit2547@gmail.com>
2024-03-25 23:30:04 +0700

    Fix sign-out button color in TopMenu component

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -33,7 +33,7 @@ export default function TopMenu() {
         ) : null}
         {session ? (
           <Link href="/api/auth/signout">
-            <div className="flex items-center h-full px-2 text-cyan-600 text—sm font-bold">
+            <div className="flex items-center h-full px-2 text-red-600 text—sm font-bold">
               Sign-Out of {session.user?.name}
             </div>
           </Link>

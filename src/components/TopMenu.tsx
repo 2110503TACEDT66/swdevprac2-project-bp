d920c6d7138511ef4289985483cbf76db3187c2a
Phudit Pumcharern <bigphudit2547@gmail.com>
2024-03-25 19:13:01 +0700

    Improve UX by adding music

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,43 +1,57 @@
+'use client'
 import Image from "next/image"
 import styles from "./topmenu.module.css"
 import TopMenuItem from "./TopMenuItem"
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import Link from "next/link"
+import { ElfsightWidget } from "next-elfsight-widget"
+import { useSession } from "next-auth/react"
 
 export default async function TopMenu() {
-  const session = await getServerSession(authOptions)
-  console.log(session)
+  const { data: session, status } = useSession();
+
+  // console.log(session)
   
   return (
     <div className={styles.menucontainer}>
-        <Link href="/">
-          <Image src={"/img/logo.png"} alt="logo" className={styles.logoimg} width={0} height={0} sizes="100vh"/>
-        </Link>
-        <TopMenuItem title="Restaurant" pageRef="/restaurant"/>
-        <TopMenuItem title="Reservation" pageRef="/reservation"/>
-        <div className="flex flex-row absolute right-0 h-full">
-          <TopMenuItem title="My Reservation" pageRef="/myreservation"/>
-          {
-                (session?.user.role == "admin")
-                ? <TopMenuItem title="Add new restaurant" pageRef="/addrestaurant"/>
-                : null
-          }
-          {
-            session ? <Link href="/api/auth/signout">
+      <Link href="/">
+        <Image
+          src={"/img/logo.png"}
+          alt="logo"
+          className={styles.logoimg}
+          width={0}
+          height={0}
+          sizes="100vh"
+        />
+      </Link>
+      <TopMenuItem title="Restaurant" pageRef="/restaurant" />
+      <TopMenuItem title="Reservation" pageRef="/reservation" />
+      <ElfsightWidget widgetId="a9b0e464-6b63-418f-89df-4532a3bf7efc"/>
+      <div className="flex flex-row absolute right-0 h-full">
+        <TopMenuItem title="My Reservation" pageRef="/myreservation" />
+        {session?.user.role == "admin" ? (
+          <TopMenuItem title="Add new restaurant" pageRef="/addrestaurant" />
+        ) : null}
+        {session ? (
+          <Link href="/api/auth/signout">
+            <div className="flex items-center h-full px-2 text-cyan-600 text—sm font-bold">
+              Sign-Out of {session.user?.name}
+            </div>
+          </Link>
+        ) : (
+          <>
+            <Link href="/auth/login">
               <div className="flex items-center h-full px-2 text-cyan-600 text—sm font-bold">
-              Sign-Out of {session.user?.name}</div></Link>
-            : (<>
-              <Link href="/auth/login">
-                <div className="flex items-center h-full px-2 text-cyan-600 text—sm font-bold">
-                Login</div></Link>
-              <Link href="/auth/register">
-                <div className="flex items-center h-full px-2 text-cyan-600 text—sm font-bold">
-                Register</div></Link>
-              </>
-              )
-          }
-        </div>
+                Login
+              </div>
+            </Link>
+            <Link href="/auth/register">
+              <div className="flex items-center h-full px-2 text-cyan-600 text—sm font-bold">
+                Register
+              </div>
+            </Link>
+          </>
+        )}
+      </div>
     </div>
-  )
+  );
 }

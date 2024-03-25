2433796fdd8f1a86753e33b7f349133e6322347e
Phudit Pumcharern <bigphudit2547@gmail.com>
2024-03-25 13:15:51 +0700

    Improve Banner component styling

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -22,31 +22,31 @@ export default function Banner() {
     >
       <Image src={covers[index]} alt="cover" fill={true} objectFit="cover" />
       {session ? (
-        <div
-          className="z-20 absolute top-5 left-10 font-semibold text-white text-xl bg-black px-5 py-2 rounded-lg"
-        >
+        <div className="z-20 absolute top-5 left-10 font-semibold text-white text-xl bg-black px-5 py-2 rounded-lg">
           Welcome back! {session.user?.name}
         </div>
       ) : null}
-      <div className={styles.bannerText} >
-        <h1 className="text-4xl font-medium mix-blend-difference">Restaurant Reservation</h1>
+      <div className={styles.bannerText}>
+        <h1 className="text-4xl font-medium mix-blend-difference">
+          Restaurant Reservation
+        </h1>
         <h3 className="text-xl font-serif">
           Discover the culinary delights of our restaurant with a reservation,
           where every dish tells a story of flavor and tradition.
         </h3>
       </div>
       <button
-        className="bg-white text-cyan-600
-          border border-cyan-600 font-semibold py-2 px-2
-          m-2 rounded z-30 absolute bottom-0 right-0
-          hover:bg-cyan-600 hover:text-white
-          hover: border-transparent"
+        className="bg-white text-red-600
+          border border-red-600 font-semibold py-2 px-2
+          m-2 rounded z-30 shadow-md absolute bottom-0 right-0
+          hover:bg-red-700 hover:text-white
+          hover: border-transparent transition-colors duration-300"
         onClick={(e) => {
           e.stopPropagation();
           router.push("/restaurant");
         }}
       >
-        Reserve your seat now
+        Choose your restaurant
       </button>
     </div>
   );

45bd4275e6e3ddf17f1a319ce343ba98ae7f1146
Phudit Pumcharern <bigphudit2547@gmail.com>
2024-03-25 21:22:29 +0700

    Refactor CardPanel component and update middleware configuration

--- src/components/CardPanel.tsx
+++ src/components/CardPanel.tsx
@@ -1,75 +1,118 @@
-"use client"
+"use client";
 import { useEffect, useReducer, useState } from "react";
 import Card from "./Card";
 import Link from "next/link";
 import getRestaurants from "@/libs/getRestaurants";
 import { RestaurantItem, RestaurantJson } from "../../interface";
+import { LinearProgress, Box } from "@mui/material";
 
 export default function CardPanel() {
-    const [restaurantResponse, setRestaurantResponse] = useState<RestaurantJson|null>(null);
+  const [restaurantResponse, setRestaurantResponse] =
+    useState<RestaurantJson | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const restaurants = await getRestaurants(page);
-            setRestaurantResponse(restaurants);
-        }
-    
-        fetchData();
-    }, [page])
-    
-    const compareReducer = ( compareList: Map<string, number>, action: { type: string, restaurantName: string, rating?: number } ) => {
-        switch (action.type) {
-            case "add": {
-                return new Map( compareList.set(action.restaurantName, action.rating || 5) );
-            }
-            case "remove": {
-                compareList.delete(action.restaurantName);
-                return new Map( compareList );
-            }
-            default: {
-                return compareList;
-            }
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const restaurants = await getRestaurants(page);
+      setRestaurantResponse(restaurants);
+      setIsLoading(false);
     };
 
-    const [ compareList, dispatchCompare ] = useReducer(compareReducer, new Map<string, number>());
+    fetchData();
+  }, [page]);
 
-    // Mock Data for Demonstration Only
-    // const mockRestaurantRepo = [
-    //     {rid: "001", name: "Chulalongkorn Restaurant", image: "/img/chula.jpg"},
-    //     {rid: "002", name: "Rajavithi Restaurant", image: "/img/rajavithi.jpg"},
-    //     {rid: "003", name: "Thammasat University Restaurant", image: "/img/thammasat.jpg"},
-    // ]
+  const compareReducer = (
+    compareList: Map<string, number>,
+    action: { type: string; restaurantName: string; rating?: number }
+  ) => {
+    switch (action.type) {
+      case "add": {
+        return new Map(
+          compareList.set(action.restaurantName, action.rating || 5)
+        );
+      }
+      case "remove": {
+        compareList.delete(action.restaurantName);
+        return new Map(compareList);
+      }
+      default: {
+        return compareList;
+      }
+    }
+  };
 
-    if (!restaurantResponse) return (<p>Restaurant Panel is Loading</p>);
+  const [compareList, dispatchCompare] = useReducer(
+    compareReducer,
+    new Map<string, number>()
+  );
 
+  if (isLoading) {
     return (
-      <div>
-        <div style={{margin: "20px", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "space-around", alignContent: "space-around"}}>
-        {
-            restaurantResponse.data.map((restaurantItem: RestaurantItem) => (
-                <Link href={`/restaurant/${restaurantItem.id}`} className="w-1/5" key={restaurantItem.id}>
-                    <Card restaurantName={restaurantItem.name} imgSrc={restaurantItem.picture}
-                        onCompare={(restaurant: string, rating: number) => dispatchCompare({type: "add", restaurantName: restaurant, rating: rating})}
-                    />
-                </Link>
-            ))
-        }
-        </div>
-        {
-            restaurantResponse.pagination.prev
-            ? <button onClick={() => {setPage(restaurantResponse.pagination.prev!.page)}}>Prev</button>
-            : null
-        }
-        <div>Page: {page}</div>
-        {
-            restaurantResponse.pagination.next
-            ? <button onClick={() => {setPage(restaurantResponse.pagination.next!.page)}}>Next</button>
-            : null
-        }
-      </div>
-    )
+      <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
+        <p>Restaurant Panel is Loading</p>
+        <LinearProgress />
+      </Box>
+    );
   }
-  
+
+  if (!restaurantResponse) {
+    return <p>No data available</p>;
+  }
+
+  return (
+    <div>
+      <div
+        style={{
+          margin: "20px",
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+          alignContent: "space-around",
+        }}
+      >
+        {restaurantResponse.data.map((restaurantItem: RestaurantItem) => (
+          <Link
+            href={`/restaurant/${restaurantItem.id}`}
+            className="w-1/5"
+            key={restaurantItem.id}
+          >
+            <Card
+              restaurantName={restaurantItem.name}
+              imgSrc={restaurantItem.picture}
+              onCompare={(restaurant: string, rating: number) =>
+                dispatchCompare({
+                  type: "add",
+                  restaurantName: restaurant,
+                  rating: rating,
+                })
+              }
+            />
+          </Link>
+        ))}
+      </div>
+      {restaurantResponse.pagination.prev ? (
+        <button
+          onClick={() => {
+            setPage(restaurantResponse.pagination.prev!.page);
+          }}
+        >
+          Prev
+        </button>
+      ) : null}
+      <div>Page: {page}</div>
+      {restaurantResponse.pagination.next ? (
+        <button
+          onClick={() => {
+            setPage(restaurantResponse.pagination.next!.page);
+          }}
+        >
+          Next
+        </button>
+      ) : null}
+    </div>
+  );
+}
